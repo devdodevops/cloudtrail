2b3218553e066bcfe31d79e3a71e633a466514fd
Dev Parmar <dev.parmar@rackspace.com>
2025-06-10 16:15:31 +0530

    first commit

---
+++ cloudtrail.tf
@@ -0,0 +1,32 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "CloudTrail-Loggining-Dev-Account"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_cloudtrail" "awscloudtrail_central_logs" {
+  name                            = var.centrail_logging_name
+  s3_bucket_name                  = aws_s3_bucket.example
+  s3_key_prefix                   = var.key_prefix
+
+  is_organization_trail           = true
+  is_multi_region_trail           = true
+  include_global_service_events   = true
+
+  # event_selector {
+  #   # This selector is for data events.
+  #   include_management_events = false
+  #   read_write_type           = "WriteOnly"
+
+  #   data_resource {
+  #     type   = "AWS::S3::Object"
+  #     values = var.s3_data_event_monitored_buckets 
+  #   }
+  # }
+
+}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+#######################################################################
+# s3 bucket variables
+#######################################################################
+variable "aws_region" {
+    default = "us-east-1"
+}
+variable "centrail_logging_name" {
+    default = "awscloudtrail-central-logging-my-account"
+}
+variable "key_prefix" {
+    default = "rHPPn1Vzzj"
+}
+
+variable "s3_data_event_monitored_buckets" {
+  description = "A list of S3 bucket ARNs (with optional prefixes) in member accounts to monitor for write data events. Each prefix should be a separate entry if monitoring multiple prefixes in the same bucket."
+  type        = list(string)
+  default = [
+    "arn:aws:s3:::dev-s3-source/state/", # Monitor specific prefix
+    "arn:aws:s3:::dev-s3-source/script/",
+    "arn:aws:s3:::dev-s3-source/nat/"
+  ]
+}
