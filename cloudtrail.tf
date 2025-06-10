e8689a68a838fa18d967eae707349c4c07d5f300
Dev Parmar <dev.parmar@rackspace.com>
2025-06-10 16:36:53 +0530

    new fix

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -9,6 +9,60 @@ resource "aws_s3_bucket" "example" {
   }
 }
 
+
+# Your S3 bucket resource
+resource "aws_s3_bucket" "example" {
+  bucket = "dev-cloudtrail-logs-dev-266735842047"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+# Constructs the IAM policy in a readable HCL format
+data "aws_iam_policy_document" "cloudtrail_s3_policy" {
+  statement {
+    sid       = "AWSCloudTrailAclCheck"
+    effect    = "Allow"
+    actions   = ["s3:GetBucketAcl"]
+    resources = [aws_s3_bucket.example.arn]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+  }
+
+  statement {
+    sid       = "AWSCloudTrailWrite"
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    # Note how we dynamically get the account ID from the data source
+    resources = ["${aws_s3_bucket.example.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+  }
+}
+
+# Attaches the policy document to the S3 bucket
+resource "aws_s3_bucket_policy" "example_policy" {
+  # References the bucket created above
+  bucket = aws_s3_bucket.example.id
+
+  # Uses the JSON output from the policy document
+  policy = data.aws_iam_policy_document.cloudtrail_s3_policy.json
+}
+
 resource "aws_cloudtrail" "awscloudtrail_central_logs" {
   name                            = var.centrail_logging_name
   s3_bucket_name                  = aws_s3_bucket.example.id
