a5a7c0219291107d89f37caf9713f79b7b411130
Dev Parmar <dev.parmar@rackspace.com>
2025-06-10 16:47:46 +0530

    data-event

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -145,4 +145,16 @@ resource "aws_cloudtrail" "awscloudtrail_central_logs" {
   is_organization_trail       = false
   is_multi_region_trail       = true
   include_global_service_events = true
+
+  event_selector {
+    # This selector is for data events.
+    include_management_events = false
+    read_write_type           = "WriteOnly"
+
+    data_resource {
+      type   = "AWS::S3::Object"
+      values = var.s3_data_event_monitored_buckets 
+    }
+  }
+
 }
